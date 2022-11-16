95afc043233263a3c48d623c4adf297e5c195202
rezaio <rezaoktario77@gmail.com>
2022-11-16 11:41:29 +0700

    Initial commit 👌

---
+++ app/src/main/java/com/reza/simpleasynctask/SimpleAsyncTask.java
@@ -0,0 +1,12 @@
+package com.reza.simpleasynctask;
+
+import android.widget.TextView;
+
+public class SimpleAsyncTask {
+    public SimpleAsyncTask(TextView mTextView) {
+    }
+
+    public void execute() {
+
+    }
+}
